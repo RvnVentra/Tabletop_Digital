f800478c9083cb181fab3b6ffa801abec695c343
Thomas Casse <AlexTcp@hotmail.com>
2020-02-18 14:57:44 -0500

    Refactored Game Manager

---
+++ Capstone/Games/Game.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone
+{
+    public class Game
+    {
+        readonly Random random = new Random();
+        public List<Card> Deck = new List<Card>();
+        public Table Table = new Table();
+
+        public List<Player> Players = new List<Player>();
+        public Dictionary<string, List<Card>> PlayerCards = new Dictionary<string, List<Card>>();
+
+        public Game()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Deck.Add(new Card { Number = 0, Color = i });
+
+                for (int j = 1; j < 13; j++)
+                {
+                    Deck.Add(new Card { Number = j, Color = i });
+                    Deck.Add(new Card { Number = j, Color = i });
+                }
+            }
+
+            ShuffleDeck();
+            Table.TopCard = DrawCard();
+        }
+
+        public void AddPlayer(int playerId, string connectionId, string name)
+        {
+            PlayerCards[connectionId] = new List<Card>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                PlayerCards[connectionId].Add(DrawCard());
+            }
+
+            Players.Add(new Player
+            {
+                PlayerId = playerId,
+                ConnectionId = connectionId,
+                Name = name,
+                CardCount = PlayerCards[connectionId].Count
+            });
+        }
+
+        public void RemovePlayer(string id)
+        {
+            try
+            {
+                Players.Remove(Players.Single(p => p.ConnectionId == id));
+                PlayerCards.Remove(id);
+            }
+            catch { }
+        }
+
+        public bool CheckTurn(string id)
+        {
+            return Players.First().ConnectionId == id;
+        }
+
+        public void ShuffleDeck()
+        {
+            Deck = Deck.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+
+        public Card DrawCard()
+        {
+            if (Deck.Count <= 0)
+                return GetRandomCard();
+
+            Card card = Deck[0];
+            Debug.Log("Card " + card.Color + ", " + card.Number + " Was Drawn");
+            Deck.RemoveAt(0);
+            return card;
+        }
+
+        public bool DrawCard(string id)
+        {
+            if (CheckTurn(id))
+            {
+                Player cPlayer = Players.Single(p => p.ConnectionId == id);
+
+                cPlayer.CardCount++;
+                PlayerCards[id].Add(DrawCard());
+
+                Players.Add(cPlayer);
+                Players.Remove(cPlayer);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Card GetRandomCard()
+        {
+            return new Card
+            {
+                Number = random.Next(0, 13),
+                Color = random.Next(0, 4)
+            };
+        }
+
+        public bool PlayCard(string playerID, int cardID)
+        {
+            if (CheckTurn(playerID))
+            {
+                Player cPlayer = Players.Single(p => p.ConnectionId == playerID);
+
+                if (PlayerCards[playerID][cardID].Color == Table.TopCard.Color ||
+                    PlayerCards[playerID][cardID].Number == Table.TopCard.Number)
+                {
+                    //Move current player to bottom of the turn order
+                    //Skips this if reverse is played
+                    if (PlayerCards[playerID][cardID].Number != 11)
+                    {
+                        Players.Add(cPlayer);
+                        Players.Remove(cPlayer);
+                    }
+
+                    //Card effects
+                    switch (PlayerCards[playerID][cardID].Number)
+                    {
+                        default:
+                            break;
+
+                        case 10: //----- Pick Up 2 -----//
+                            Debug.Log(cPlayer.Name + " Played Pick Up 2");
+
+                            if (PlayerCards.Count > 1)
+                            {
+                                Players[0].CardCount += 2;
+                                PlayerCards[Players[0].ConnectionId].Add(DrawCard());
+                                PlayerCards[Players[0].ConnectionId].Add(DrawCard());
+                            }
+                            break;
+
+                        case 11://----- Reverse -----//
+                            Debug.Log(cPlayer.Name + " Played Reverse");
+                            Players.Reverse();
+                            break;
+
+                        case 12://----- Skip -----//
+                            Debug.Log(cPlayer.Name + " Played Skip");
+                            Players.Add(Players[0]);
+                            Players.Remove(Players[0]);
+                            break;
+                    }
+
+                    //Update card count
+                    cPlayer.CardCount--;
+
+                    //Played card becomes top card
+                    Table.TopCard = PlayerCards[playerID][cardID];
+                    PlayerCards[playerID].RemoveAt(cardID);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+    }
+
+    public class Table
+    {
+        public Card TopCard { get; set; }
+    }
+
+    public class Card
+    {
+        public int Number { get; set; }
+        public int Color { get; set; }
+    }
+}
---
+++ Capstone/Hubs/GameHub.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace Capstone
+{
+    public class GameHub : Hub
+    {
+        readonly GameManager GM = GameManager.Instance;
+        readonly ChatManager CM = ChatManager.Instance;
+        readonly string[] COLORS = new string[] { "Blue", "Red", "Green", "Yellow" };
+
+        public static ConcurrentDictionary<string, User> Users = new ConcurrentDictionary<string, User>();
+
+        public override Task OnConnectedAsync()
+        {
+            Debug.Log("User Connected: " + Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            GM.Games["GAME_CODE"].RemovePlayer(Context.ConnectionId);
+            UpdatePlayerList();
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        //---------------- Update Methods ------------------
+
+        public async void UpdateTable()
+        {
+            await Clients.All.SendAsync("UpdateTable", GM.Games["GAME_CODE"].Table);
+        }
+
+        public async void UpdateHand()
+        {
+            await Clients.Caller.SendAsync("UpdateHand", GM.Games["GAME_CODE"].PlayerCards[Context.ConnectionId]);
+        }
+
+        public async void UpdateOtherHand(string connectionId)
+        {
+            await Clients.Client(connectionId).SendAsync("UpdateHand", GM.Games["GAME_CODE"].PlayerCards[connectionId]);
+        }
+
+        public async void UpdateChat()
+        {
+            await Clients.All.SendAsync("UpdateChat", CM.ChatBox);
+        }
+
+        public async void UpdatePlayerList()
+        {
+            if(GM.Games["GAME_CODE"].Players.Count > 0)
+                TableStatus(GM.Games["GAME_CODE"].Players[0].Name + "'s Turn");
+
+            await Clients.All.SendAsync("UpdatePlayerList", GM.Games["GAME_CODE"].Players);
+        }
+
+        //------------- Status Text Methods --------------
+
+        public async void TableStatus(string text)
+        {
+            await Clients.All.SendAsync("TableStatus", text);
+        }
+
+        public async void TableSubStatus(string text)
+        {
+            await Clients.All.SendAsync("TableSubStatus", text);
+        }
+
+        public async void HandStatus(string text)
+        {
+            await Clients.Caller.SendAsync("HandStatus", text);
+        }
+
+        //---------------- Call Methods ------------------
+
+        public async void EnterName(string name)
+        {
+            int playerID = Users.Count + 1;
+
+            name = name.Trim();
+
+            if(name == "")
+            {
+                try
+                {
+                    Random rand = new Random();
+                    name = NameList.NameArray[rand.Next(NameList.NameArray.Length)];
+                }
+                catch
+                {
+                    name = "Anonymous";
+                }
+            }
+                
+            Users.TryAdd(Context.ConnectionId, new User()
+            {
+                ConnectionId = Context.ConnectionId,
+                Username = name,
+                PlayerId = playerID
+            });
+
+            GM.Games["GAME_CODE"].AddPlayer(playerID, Context.ConnectionId, name);
+            await Clients.Caller.SendAsync("JoinGame", playerID);
+        }
+
+        public void DrawCard()
+        {
+            if (GM.Games["GAME_CODE"].DrawCard(Context.ConnectionId))
+            {
+                TableSubStatus(Users[Context.ConnectionId].Username + " Drew a Card");
+                UpdatePlayerList();
+                UpdateHand();
+            }
+        }
+
+        public void PlayCard(int cardID)
+        {
+            Card pCard = GM.Games["GAME_CODE"].PlayerCards[Context.ConnectionId][cardID];
+
+            if (GM.Games["GAME_CODE"].PlayCard(Context.ConnectionId, cardID))
+            {
+                switch (pCard.Number)
+                {
+                    default:
+                        TableSubStatus(
+                            Users[Context.ConnectionId].Username + " Played " 
+                            + COLORS[pCard.Color] + " " + pCard.Number);
+                        break;
+
+                    case 10: //----- Pick Up 2 -----//
+                        UpdateOtherHand(GM.Games["GAME_CODE"].Players.First().ConnectionId);
+                        break;
+                }
+
+                UpdatePlayerList();
+                UpdateTable();
+                UpdateHand();
+            }
+        }
+
+        public void Chat(string input)
+        {
+            CM.NewMessage(Users[Context.ConnectionId].Username, input);
+
+            UpdateChat();
+        }
+    }
+
+    public class User
+    {
+        public string ConnectionId { get; set; }
+        public int PlayerId { get; set; }
+        public string Username { get; set; }
+    }
+}
